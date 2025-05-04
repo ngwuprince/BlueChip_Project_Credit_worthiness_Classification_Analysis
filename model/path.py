956965fd8eb666700ad03133adddca558630b229
Ogbonna Ngwu <48644277+ngwuprince@users.noreply.github.com>
2025-05-04 13:03:26 +0100

    Add files via upload

---
+++ model/path.py
@@ -0,0 +1,9 @@
+import os
+
+# Base directory (correctly formatted)
+P_DATA = os.path.join('C:\\', 'Users', 'User', 'Desktop', 'Kaggle_Playgound_series', 'bluechip_credit_worthiness_analysis')
+
+# File paths
+P_TRAIN_DATA = os.path.join(P_DATA, 'Train.csv')
+P_TEST_DATA = os.path.join(P_DATA, 'Test.csv')
+P_SAMPLE_SUB = os.path.join(P_DATA, 'Sample Submission.csv')
